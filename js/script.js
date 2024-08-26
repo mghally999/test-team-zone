63a708d3d11054366348e70a8966580aeb1700ac
Mohammed Ghally <mohammedghallydev@gmail.com>
2024-08-26 13:28:33 +0400

    created Gallery 2

--- js/script.js
+++ js/script.js
@@ -147,3 +147,87 @@ const slider = function () {
   });
 };
 slider();
+
+const slider6th = function () {
+  const slides = document.querySelectorAll(".slide-6th");
+  const btnLeft = document.querySelector(".slider__btn--left-6th");
+  const btnRight = document.querySelector(".slider__btn--right-6th");
+  const dotContainer = document.querySelector(".dots-6th");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot-6th" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot-6th")
+      .forEach((dot) => dot.classList.remove("dots__dot--active-6th"));
+
+    document
+      .querySelector(`.dots__dot-6th[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active-6th");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot-6th")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider6th();
